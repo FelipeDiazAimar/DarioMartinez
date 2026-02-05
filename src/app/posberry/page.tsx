7016aac1ac70d7814bc6ef38b5bd984a2f3577e6
Dario Martinez Computacion <DMartinezComputacion@gmail.com>
2026-02-05 15:01:33 +0000

    hacela un poco mas grande a la imagen de la pc

--- src/app/posberry/page.tsx
+++ src/app/posberry/page.tsx
@@ -140,8 +140,8 @@ export default function PosberryPage() {
               <Image
                 src="/POSBERRY2.png"
                 alt="POSBerry en una computadora"
-                width={320}
-                height={240}
+                width={380}
+                height={285}
                 className=""
               />
             </div>
