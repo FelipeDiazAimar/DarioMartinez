255ae0cf400f0c8a53adeff1ac53175dc70be6f4
Dario Martinez Computacion <DMartinezComputacion@gmail.com>
2026-02-06 13:54:05 +0000

    por para ver o editar su contenido (_for element p_)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -63,7 +63,7 @@ export default function AdminPage() {
                     Panel de Administración
                 </h1>
                 <p className="text-muted-foreground">
-                    Seleccione una sección para editar su contenido.
+                    Seleccione una sección para ver o editar su contenido.
                 </p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
